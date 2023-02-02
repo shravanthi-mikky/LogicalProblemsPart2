dd68d4985290a9c6c1cf0cdba030a8c2e20fb5bf
shravanthi-mikky <shravanthi27041996@gmail.com>
2023-02-02 16:28:07 +0530

    [shravanthi] - Vending Machine

---
+++ Logical4AR/Pyramid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical4AR
+{
+    public class Pyramid
+    {
+        public static void PyramidFormation(int[] InputArray)
+        {
+            Array.Sort(InputArray);
+
+            List<int> even = new List<int>();
+            List<int> odd = new List<int>();
+            foreach (var Input in InputArray)
+            {
+                if(Input%2 == 0)
+                {
+                    even.Add(Input);
+                }
+                else
+                {
+                    odd.Add(Input);
+                }
+            }
+        }
+    }
+}
